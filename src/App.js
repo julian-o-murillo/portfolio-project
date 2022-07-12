abf9ab0b338bc6231e81487564ddf169ff43604b
Julian Murillo <julianmurillo@Julians-MBP.attlocal.net>
2022-07-12 17:33:42 -0400

    adds about me component and styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/navbar/navbar.component';
 import Header from './components/header/header.component';
+import AboutMe from './components/about-me/about-me.component';
 
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
@@ -26,88 +27,9 @@ function App() {
 
   return (
     <Fragment>
-      <Particles
-        className='particles-canvas'
-        canvasClassName='tsparticles-canvas-el'
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: '#0d47a1',
-            image: "url('wall-and-laptop-background.jpg')",
-            position: 'absolute',
-            repeat: 'no-repeat',
-            size: 'cover',
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: 'push',
-              },
-              onHover: {
-                enable: true,
-                mode: 'repulse',
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 100,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: '#ffffff',
-            },
-            links: {
-              color: '#ffffff',
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: 'none',
-              enable: true,
-              outModes: {
-                default: 'bounce',
-              },
-              random: false,
-              speed: 3,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 900,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: 'circle',
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
       <Navbar />
       <Header />
+      <AboutMe />
     </Fragment>
   );
 }
